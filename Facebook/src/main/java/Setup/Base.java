0e7074bf10c68f4e6b27d791154ff6beefb2a7cf
Amrut2395 <117375442+Amrut2395@users.noreply.github.com>
2022-11-24 22:19:10 +0530

    Update Base.java

--- Facebook/src/main/java/Setup/Base.java
+++ Facebook/src/main/java/Setup/Base.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
-public class Base {
+public class Base1 {
 	
 	public static WebDriver openChromeBrowser ()
 	{
